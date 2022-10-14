c52778362e64927ee195de051970cec9c60c28dd
Wudong Liu <wudong.liu@gmail.com>
2022-10-14 22:44:37 +0100

    update schema for events.

--- initdb/002_trigger.sql
+++ initdb/002_trigger.sql
@@ -5,27 +5,52 @@ create or replace function competition_entry_insert_trigger_fn() returns trigger
 as
 $$
 DECLARE
-    entry_limit integer;
-    current_entry_count integer;
+    v_competition competitions;
+    v_current_entry_count integer;
+    v_exist_entry_count integer;
 BEGIN
     -- we don't update the competition table, but requires a lock to prevent
     -- two concurrent inserts from exceeding the limit.
-    SELECT competitions.entry_limit INTO entry_limit
-    FROM competitions where NEW.competition = competitions.id FOR UPDATE;
+    SELECT * into v_competition
+    FROM competitions cc where NEW.competition = cc.id FOR UPDATE;
 
-    SELECT count(*) INTO current_entry_count
+    -- if it is a single entry, make sure it is not entered again.
+    if v_competition.type <> 'single' then
+        select count(*) into v_exist_entry_count from competition_entries join players p on competition_entries.player = p.id;
+        if v_exist_entry_count > 0 then
+            raise exception 'Player has already entered into the competition';
+        end if;
+    end if;
+
+    -- if it is team entry, make sure it does not exist in other team already.
+    if v_competition.type <> 'single' then
+        select count(*) into v_exist_entry_count from competition_entries
+            join teams t on competition_entries.team = t.id
+            join team_players tp on t.id = tp.team
+            where tp.player = any(
+                select tp2.player from teams t2 join team_players tp2 on t2.id = tp2.team
+                    where t2.id = new.team
+           );
+        if v_exist_entry_count > 0 then
+            raise exception 'Player has already entered into the competition in other team/doubles';
+        end if;
+    end if;
+
+    SELECT count(*) INTO v_current_entry_count
     FROM competition_entries WHERE competition = NEW.competition and withdrawn = false;
 
-    IF (entry_limit > 0 AND current_entry_count >= entry_limit) THEN
+    IF (v_competition.entry_limit > 0 AND v_current_entry_count >= v_competition.entry_limit) THEN
         RAISE EXCEPTION 'Competition entry limit reached';
-    ELSE
-        NEW.entry_number = current_entry_count + 1;
-        update competitions set current_entries = current_entry_count + 1
-        where NEW.competition = competitions.id;
-    END IF;
+    end if;
+
+    NEW.entry_number = v_current_entry_count + 1;
+    NEW.competition_type = v_competition.type;
+
+    update competitions set current_entries = v_current_entry_count + 1
+    where NEW.competition = competitions.id;
 
     RETURN NEW;
-END;
+END
 $$;
 
 create trigger competition_entry_insert_trigger
