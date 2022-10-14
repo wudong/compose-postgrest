f2251f343bc6aee53c58f260976c246b5b75300e
Wudong Liu <wudong.liu@gmail.com>
2022-10-14 08:06:43 +0100

    add all

--- initdb/005_function_rpc.sql
+++ initdb/005_function_rpc.sql
@@ -1,6 +1,7 @@
 -- given the list of players, find the team that
 -- contains all the players.
-create or replace function get_team_by_players(players integer[], is_double boolean)
+-- IMPORTANT, it is possible to have a two player team that is not a double.
+create or replace function get_team_by_players(v_players integer[], v_is_double boolean)
     returns
         table
         (
@@ -17,74 +18,63 @@ select t.id, t.gender, t.ranking, array_agg(p) as players
 from teams t
          join team_players tp on t.id = tp.team
          join players p on tp.player = p.id
-where t.is_double = is_double and (tp.player in players)
+where t.is_double = v_is_double and (p.id = any(v_players))
 group by t.id
-having array_length(array_agg(p), 1) >= array_length(players, 1);
+having array_length(array_agg(p), 1) >= array_length(v_players, 1);
 $$;
 
-create or replace function get_double_gender(gender_1 gender, gender_2 gender) returns gender_restriction
+-- get a team's gender based on its players' gender.
+create or replace function get_team_gender(v_genders gender[]) returns gender_restriction
     language sql
     IMMUTABLE
 as
 $$
-select case gender_1 = gender_2
-       when
-           true then 'mix'::gender_restriction
-       else
-           gender_1::text::gender_restriction
-       end;
+    select case
+           when t.has_male and not t.has_female then 'male'::gender_restriction
+           when not t.has_male and t.has_female then 'female'::gender_restriction
+           else 'mix'::gender_restriction
+        end
+    from (select 'male' = any (v_genders) as has_male, 'female' = any (v_genders) as has_female) as t;
 $$;
 
-create or replace function create_double(player_1 integer, player_2 integer) returns integer
+-- create a team.
+drop function if exists create_team;
+create or replace function create_team(v_players  integer[], v_is_double boolean) returns integer
     language plpgsql
 as
 $$
 declare
-    team_id  integer;
-    genders gender[];
-    gender   gender_restriction;
+    v_all_player_exists integer;
+    v_team_id  integer;
+    v_player_id  integer;
+    v_gender   gender_restriction;
 begin
-    select id into team_id from get_team_by_players(array(player_1, player_2), true);
-    IF team_id is NULL then
-        select array_agg(p.gender) into genders from players p where p.id = player_1 OR p.id = player_2;
-
-        if array_length(genders, 1) <> 2 then
-            raise exception 'players must be two';
-        end if;
-
-        insert into teams(is_double, gender) values (true, get_double_gender(genders[1], genders[2]))
-                                             returning id into team_id;
-        insert into team_players(team, player) values (team_id, player_1);
-        insert into team_players(team, player) values (team_id, player_2);
+    if array_length(v_players, 1) <= 1 then
+        raise exception 'team must more than 1 player';
     end if;
-    return team_id;
+
+    if array_length(v_players, 1) <> 2 and v_is_double then
+        raise exception 'double can only has two players';
+    end if;
+
+    -- verify all player_id exists.
+    select array_length(array_agg(id), 1) into v_all_player_exists from players where id = any(v_players);
+    if v_all_player_exists <> array_length(v_players, 1) then
+        raise exception 'some player_id not exists';
+    end if;
+
+    select id into v_team_id from get_team_by_players(v_players, v_is_double);
+
+    if v_team_id is NULL then
+        select get_team_gender(array_agg(p.gender)) into v_gender from players p where p.id = any(array[1,2,3]);
+
+        insert into teams(is_double, gender) values (v_is_double, v_gender) returning id into v_team_id;
+
+        foreach v_player_id in array v_players
+        loop
+            insert into team_players(team, player) values (v_team_id, v_player_id);
+        end loop;
+    end if;
+    return v_team_id;
 end
 $$
-
-
-create or replace function create_team(players players[]) returns integer
-    language plpgsql
-as
-$$
-declare
-team_id  integer;
-    genders gender[];
-    gender   gender_restriction;
-begin
-select id into team_id from get_double(player_1, player_2);
-IF team_id is NULL then
-select array_agg(p.gender) into genders from players p where p.id = player_1 OR p.id = player_2;
-
-if array_length(genders, 1) <> array_length(players, 1) then
-            raise exception 'not all players exists';
-end if;
-
-insert into teams(is_double, gender) values (true, get_double_gender(genders[1], genders[2]))
-    returning id into team_id;
-insert into team_players(team, player) values (team_id, player_1);
-insert into team_players(team, player) values (team_id, player_2);
-end if;
-return team_id;
-end
-$$
-
