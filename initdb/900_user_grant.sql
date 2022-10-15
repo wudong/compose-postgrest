b21f1d7a27a7a69d09e449ed95073ae0aa15a2e4
Wudong Liu <wudong.liu@gmail.com>
2022-10-15 09:12:55 +0100

    update trigger for entry.

--- initdb/900_user_grant.sql
+++ initdb/900_user_grant.sql
@@ -1,10 +1,11 @@
-drop schema if exists public CASCADE;
-CREATE SCHEMA public;
+-- CREATE SCHEMA public;
 
-drop ROLE IF EXISTS anno;
 CREATE ROLE anon;
 
 GRANT USAGE ON SCHEMA public TO anon;
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO anon;
 GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO anon;
 GRANT SELECT, INSERT, UPDATE ON ALL TABLES IN SCHEMA public TO anon;
+
+-- need to execute this last, cause newly created table does not have the permission from the grant.
+-- https://dba.stackexchange.com/questions/33943/granting-access-to-all-tables-for-a-user
